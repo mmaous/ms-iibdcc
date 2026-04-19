41c81c10544146bb3fea59c818016762e0e66035
mhand <mhandmaous@gmail.com>
2026-04-19 16:10:05 +0100

    tp(archi-jee): TP2 - Q1 - init project

---
+++ m1/s1/archi-jee-system-distribue/TP2 - ORM JPA Hibernate Spring Data/product-hub/src/main/java/ma/enset/producthub/ProductHubApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.producthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductHubApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProductHubApplication.class, args);
+  }
+
+}
---
+++ m1/s1/archi-jee-system-distribue/TP2 - ORM JPA Hibernate Spring Data/product-hub/src/test/java/ma/enset/producthub/ProductHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.producthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductHubApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
