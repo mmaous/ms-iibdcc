6f320e3fca9464e7db83e90a4628642452295387
mhand <mhandmaous@gmail.com>
2026-06-13 18:18:59 +0100

    tp(archi-jee): TP3 - Q4. Test la couche DAO

---
+++ m1/s1/archi-jee-system-distribue/TP3/spring-mvc-products/README.md
@@ -0,0 +1,4 @@
+# Activité Pratique N°3 
+
+### Q4 - Tester la couche DAO?
+![test](./screenshots/test-couche-dao.png)
--- m1/s1/archi-jee-system-distribue/TP3/spring-mvc-products/src/main/java/ma/enset/springmvcproducts/SpringMvcProductsApplication.java
+++ m1/s1/archi-jee-system-distribue/TP3/spring-mvc-products/src/main/java/ma/enset/springmvcproducts/SpringMvcProductsApplication.java
@@ -1,13 +1,30 @@
 package ma.enset.springmvcproducts;
 
+import ma.enset.springmvcproducts.entities.Product;
+import ma.enset.springmvcproducts.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringMvcProductsApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringMvcProductsApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner start(ProductRepository productRepository) {
+    return args -> {
+      productRepository.save(Product.builder().name("Computer").price(4300).quantity(3).build());
+      productRepository.save(Product.builder().name("Printer").price(1200).quantity(4).build());
+      productRepository.save(Product.builder().name("Smartphone").price(3200).quantity(32).build());
+
+      productRepository.findAll().forEach(p -> {
+        System.out.println(p.getName());
+      });
+    };
   }
 
 }
