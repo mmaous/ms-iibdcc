121a99e8573237bd3f462ab7d652e995ac321599
mhand <mhandmaous@gmail.com>
2026-06-13 18:07:17 +0100

    tp(archi-jee): TP3 - Q1. project setup + maven deps

---
+++ m1/s1/archi-jee-system-distribue/TP3/spring-mvc-products/src/main/java/ma/enset/springmvcproducts/SpringMvcProductsApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.springmvcproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcProductsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMvcProductsApplication.class, args);
+  }
+
+}
