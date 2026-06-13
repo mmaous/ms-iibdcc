ba201c2d90b3ddf0f58e78cde8259c7f4c1f9cd3
mhand <mhandmaous@gmail.com>
2026-06-13 18:10:39 +0100

    tp(archi-jee): TP3 - Q2. create Product JPA entity

---
+++ m1/s1/archi-jee-system-distribue/TP3/spring-mvc-products/src/main/java/ma/enset/springmvcproducts/entities/Product.java
@@ -0,0 +1,34 @@
+package ma.enset.springmvcproducts.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotEmpty
+  @Size(min = 3, max = 50)
+  private String name;
+
+  @Min(0)
+  private double price;
+
+  @Min(1)
+  private double quantity;
+}
